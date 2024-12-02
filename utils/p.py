e133901615ccd2a5211fca397cb34d9147c047a4
nachovigilante <nachovigilante@gmail.com>
2024-12-02 16:06:08 -0300

    Muevo ps a archivo

--- utils/p.py
+++ utils/p.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
 
 def ps(vec_imgs, vec_masks):
     # histogramas
@@ -33,3 +36,55 @@ def ps(vec_imgs, vec_masks):
     p_ab = p_ab / pxs
 
     return (p_la, p_lb, p_ab)
+
+
+def p_lab(l, a, b, p1, p2, p3):
+    l_ind = math.floor(24 * l / 101)
+    a_ind = math.floor(24 * (a + 110) / 221)
+    b_ind = math.floor(24 * (b + 110) / 221)
+    return p1[l_ind, a_ind] * p2[l_ind, b_ind] * p3[a_ind, b_ind]
+
+
+def print_ps(p1, p2, p3):
+    # documentación matplotlib
+    fig, ax = plt.subplots(1, 3, subplot_kw={"projection": "3d"}, figsize=(10, 10))
+    # fig, axes = plt.subplots(2, 4, figsize=(15, 8))
+
+    # Make data.
+    X = np.arange(0, 24, 1)
+    Y = np.arange(0, 24, 1)
+    X, Y = np.meshgrid(X, Y)
+    R = (X, Y)
+    Z1 = p1[R]
+    Z2 = p2[R]
+    Z3 = p3[R]
+
+    # Plot the surface.
+    surf1 = ax[0].plot_surface(
+        Y, X, Z1, cmap=cm.coolwarm, linewidth=0, antialiased=False
+    )
+
+    surf2 = ax[1].plot_surface(
+        X, Y, Z2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+    )
+
+    surf3 = ax[2].plot_surface(
+        X, Y, Z3, cmap=cm.coolwarm, linewidth=0, antialiased=False
+    )
+
+    ax[0].set_xlabel("a")
+    ax[0].set_ylabel("l")
+    ax[0].invert_xaxis()
+    ax[0].invert_yaxis()
+
+    ax[1].set_xlabel("L")
+    ax[1].set_ylabel("b")
+    ax[1].invert_yaxis()
+
+    ax[2].set_xlabel("a")
+    ax[2].set_ylabel("b")
+    ax[2].invert_yaxis()
+    # Add a color bar which maps values to colors.
+    # fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
